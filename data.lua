d0aa238ad0e79084dec44a7063455a1d15480936
BarracudaLake <98210218+BarracudaLake@users.noreply.github.com>
2023-01-29 19:06:08 +0100

    from json to lua change

---
+++ data.lua
@@ -0,0 +1,28 @@
+return {
+  ozone = {
+    potions = {
+      TestPotion = {
+        price = 1.5,
+        desc = "This is a test."
+      }
+    },
+    food = {
+      TestFood1 = {
+        price = 1.25,
+        desc = "This is a test."
+      },
+      TestFood2 = {
+        price = 1,
+        desc = "This is a test."
+      }
+    }
+  },
+  bta = {
+    cars = {
+      TestCar = {
+        price = 30,
+        desc = "Test Car"
+      }
+    }
+  }
+}
